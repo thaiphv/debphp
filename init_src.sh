0b0066af486960d983c9430e67934bcca97ebac4
Joshua Spence <josh@joshuaspence.com>
2014-12-11 08:14:48 +1100

    Minor tidying

--- init_src.sh
+++ init_src.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#This script must be run under the directory it exists
-#Run this script after you download the php source code from php.net
-#and extract the source code to dist directory
+# This script must be run under the directory it exists. Run this script after
+# you download the PHP source code from php.net and extract the source code to
+# dist directory.
 
 WORK_DIR=`pwd`
 
@@ -18,9 +18,13 @@ wget http://pecl.php.net/get/proctitle-0.1.2.tgz
 wget http://pecl.php.net/get/ncurses-1.0.2.tgz
 wget http://pecl.php.net/get/xhprof-0.9.3.tgz
 
-for i in *tgz; do tar xzf $i; done
+for i in *tgz; do
+    tar xzf $i
+done
 
-ls -1 |grep -E ".*-([[:digit:]]+\.+){2}"|grep -v tgz| while read i ; do mv $i `echo $i | cut -d\- -f 1` ; done
+ls -1 | grep -E ".*-([[:digit:]]+\.+){2}" | grep -v tgz | while read i; do
+    mv $i `echo $i | cut -d\- -f 1`
+done
 
 mv APC apc
 
