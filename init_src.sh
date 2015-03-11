dab9d4ccce7478e5788a3518325122301276440c
Joshua Spence <josh@freelancer.com>
2015-03-11 12:12:51 +1100

    Don't statically compile GD
    Summary: Ref T17162. Don't statically compile the [[http://php.net/manual/en/book.image.php | GD]] extension into `php-f8r-app`.
    
    Test Plan: See D17759.
    
    Reviewers: patrick.cadelina, raymond.huang
    
    Reviewed By: raymond.huang
    
    Maniphest Tasks: T17162
    
    Differential Revision: https://phabricator.freelancer.com/D17710

--- init_src.sh
+++ init_src.sh
@@ -2,6 +2,10 @@
 
 set -o errexit
 
+function remove_php_extension() {
+    rm -rf "dist/ext/$1"
+}
+
 function extract_php_extension() {
     INPUT="http://pecl.php.net/get/$1.tgz"
     OUTPUT="dist/ext/${1%-*}"
@@ -16,6 +20,7 @@ mkdir dist/
 wget -O- https://github.com/php/php-src/archive/php-5.3.29.tar.gz | tar --strip-components 1 -zx -C dist
 wget http://pear.php.net/install-pear-nozlib.phar -P dist/pear/
 
+remove_php_extension gd
 extract_php_extension oauth-1.2.3
 extract_php_extension memcache-2.2.7
 extract_php_extension proctitle-0.1.2
