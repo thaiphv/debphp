228768b65ced51ae09ccd433c8a33b245879e2e2
Joshua Spence <josh@joshuaspence.com>
2015-03-17 07:10:35 +1100

    Don't statically compile proctitle
    Summary: Ref T17162. Ref T21834. Don't statically compile the [[http://php.net/manual/en/book.proctitle.php | proctitle]] extension into `php-f8r-app`.
    
    Test Plan: See D17924.
    
    Reviewers: raymond.huang
    
    Reviewed By: raymond.huang
    
    Maniphest Tasks: T17162, T21834
    
    Differential Revision: https://phabricator.freelancer.com/D17922

--- init_src.sh
+++ init_src.sh
@@ -6,15 +6,6 @@ function remove_php_extension() {
     rm -rf "dist/ext/$1"
 }
 
-function extract_php_extension() {
-    INPUT="http://pecl.php.net/get/$1.tgz"
-    OUTPUT="dist/ext/${1%-*}"
-
-    rm -rf "$OUTPUT"
-    mkdir "$OUTPUT"
-    wget -O- "$INPUT" | tar --strip-components 1 -zx -C "$OUTPUT"
-}
-
 rm -rf dist/
 mkdir dist/
 wget -O- https://github.com/php/php-src/archive/php-5.3.29.tar.gz | tar --strip-components 1 -zx -C dist
@@ -28,7 +19,6 @@ remove_php_extension mysqli
 remove_php_extension mysqlnd
 remove_php_extension tidy
 remove_php_extension xmlrpc
-extract_php_extension proctitle-0.1.2
 
 pushd dist
 PHP_AUTOCONF=autoconf2.59 ./buildconf --force
